43af2dac1b5d3d797022f98d91341f5f6e2d82f0
Duncan Horne <duncan.edwardhorne@gmail.com>
2025-04-06 14:44:22 +0100

    Initial Commit

---
+++ Mastodon Bookmarks/Mastodon_BookmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mastodon_BookmarksApp.swift
+//  Mastodon Bookmarks
+//
+//  Created by Duncan Horne on 06/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mastodon_BookmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mastodon BookmarksTests/Mastodon_BookmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mastodon_BookmarksTests.swift
+//  Mastodon BookmarksTests
+//
+//  Created by Duncan Horne on 06/04/2025.
+//
+
+import Testing
+@testable import Mastodon_Bookmarks
+
+struct Mastodon_BookmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
